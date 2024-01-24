f4008bb83123bb94e0b655add84483d48275f7a1
taneez <ty491@snu.edu.in>
2024-01-24 16:20:46 +0530

    python file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SE_LAB_2
 Software Engineering Lab Assignment 2
 This is for Assignment 2 to clone and update repository.
+
+Creating branch 1
---
+++ sort_emp_table.py
@@ -0,0 +1,28 @@
+#sorting Employee table
+emp = [
+    ['161E90', 'Ramu', 35, 59000],
+    ['171E22', 'Tejas', 30, 82100],
+    ['171G55', 'Abhi', 25, 100000],
+    ['152K46', 'Jaya', 32, 85000]
+]
+
+class Emp:
+    def __init__(self, id, name, age, salary):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.emp = []
+
+    def insert(self, id, name, age, salary):
+
+    def sort(self, choice):
+
+    
+print("Select based on what you want to sort:\n1) Age\n2) Name\n3)Salary")
+choice = int(input())
+if choice == 1:
+
+    for i, v in enumerate(emp):
+
+ 
